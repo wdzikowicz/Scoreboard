0c1c761bd8b9eb1203bc068c9d7d0953f17d2ad5
wojciech dzikowicz <wojciech.dzikowicz@gmail.com>
2022-09-06 00:18:01 +0200

    Game model

--- Scoreboard/Game.cs
+++ Scoreboard/Game.cs
@@ -4,6 +4,16 @@ namespace Scoreboard
 {
   public class Game
   {
-
-  }
+    public string HomeTeam { get; set; }
+    public string AwayTeam { get; set; }
+    public int HomeScore { get; set; }
+    public int AwayScore { get; set; }
+    public int TotalScore => HomeScore + AwayScore;
+ 
+    public Game(string homeTeam, string awayTeam)
+    {
+         this.HomeTeam = homeTeam;
+         this.AwayTeam = awayTeam;
+    }
+   }
 }
